fd7a932aeebb8c9d9e47130f27ea9c2cc73c239e
joan.tolos <joan.tolos@thomsonreuters.com>
2016-04-22 16:07:59 +0200

    Duty service property file

--- src/main/java/com/dutytrail/frontend/api/remote/DutyClient.java
+++ src/main/java/com/dutytrail/frontend/api/remote/DutyClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient("http://dt-service-duty.herokuapp.com")
+@FeignClient("dtserviceduty")
 public interface DutyClient {
 
     @RequestMapping(method = RequestMethod.GET, value = "/ping", produces = "application/json")
