ddfe2631bf4bcd4502f5547dce2eb5adf1e69061
Joan Tolos <joan.tolos@thomsonreuters.com>
2016-06-10 17:25:37 +0200

    Trying to fix the bug

--- src/main/java/com/dutytrail/frontend/api/starter/Api.java
+++ src/main/java/com/dutytrail/frontend/api/starter/Api.java
@@ -1,7 +1,5 @@
 package com.dutytrail.frontend.api.starter;
 
-import com.dutytrail.frontend.api.server.ApiService;
-import com.netflix.config.ConfigurationManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
@@ -9,24 +7,15 @@ import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-import java.lang.reflect.Method;
-
 @Configuration
 @EnableAutoConfiguration
 @EnableEurekaClient
 @ComponentScan(basePackages = {"com.dutytrail.frontend.api"})
-@EnableFeignClients(basePackages = {"com.dutytrail.frontend.api"})
+@EnableFeignClients(basePackages = {"com.dutytrail"})
 public class Api {
 
     public static void main(String[] args) {
-        configureHystrix();
         SpringApplication.run(Api.class, args);
-    }
-
-    private static void configureHystrix() {
-        for (Method method : ApiService.class.getMethods()) {
-            ConfigurationManager.getConfigInstance().setProperty(String.format("hystrix.command.%s.execution.isolation.thread.timeoutInMilliseconds", method.getName()), 0);
-        }
     }
 
 }
