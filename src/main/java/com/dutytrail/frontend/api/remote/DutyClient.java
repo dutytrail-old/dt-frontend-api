80b7484b52c0959635d586c2531ef0a1e96458fb
joan.tolos <joan.tolos@thomsonreuters.com>
2016-06-11 07:47:09 +0200

    Defined port for service duty

--- src/main/java/com/dutytrail/frontend/api/remote/DutyClient.java
+++ src/main/java/com/dutytrail/frontend/api/remote/DutyClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient(serviceId = "${feign.name}", url = "${feign.url}")
+@FeignClient(serviceId = "${dtserviceduty.name}", url = "${dtserviceduty.url}")
 public interface DutyClient {
 
     @RequestMapping(method = RequestMethod.GET, value = "/ping", produces = "application/json")
