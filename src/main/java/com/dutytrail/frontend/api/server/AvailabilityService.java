7a31ae9731584f60f3dc3cae622008b1f14c1588
joan.tolos <joan.tolos@thomsonreuters.com>
2016-06-13 15:40:09 +0200

    Api operation placeholder

--- src/main/java/com/dutytrail/frontend/api/server/AvailabilityService.java
+++ src/main/java/com/dutytrail/frontend/api/server/AvailabilityService.java
@@ -1,5 +1,6 @@
 package com.dutytrail.frontend.api.server;
 
+import com.dutytrail.frontend.api.entity.Status;
 import com.dutytrail.frontend.api.remote.DutyClient;
 import com.dutytrail.frontend.api.remote.TrailClient;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +10,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.ws.rs.core.MediaType;
-import javax.xml.bind.annotation.*;
 
 @RestController
 public class AvailabilityService {
@@ -26,33 +26,6 @@ public class AvailabilityService {
     @RequestMapping(value = "/status", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON)
     public Status status() {
         return new Status(this.dutyClient.ping(), this.trailClient.ping(), this.configPingAlive);
-    }
-
-    @XmlRootElement(name = "status")
-    @XmlAccessorType(XmlAccessType.FIELD)
-    @XmlType(propOrder = {"dutyServicePing","dutyServicePing","configPing"})
-    private class Status{
-        @XmlElement(name = "duty") private String dutyServicePing;
-        @XmlElement(name = "trail") private String trailServicePing;
-        @XmlElement(name = "config") private String configPing;
-
-        Status(String dutyServicePing, String trailServicePing, String configPing) {
-            this.dutyServicePing = dutyServicePing;
-            this.trailServicePing = trailServicePing;
-            this.configPing = configPing;
-        }
-
-        public String getDutyServicePing() {
-            return dutyServicePing;
-        }
-
-        public String getTrailServicePing() {
-            return trailServicePing;
-        }
-
-        public String getConfigPing() {
-            return configPing;
-        }
     }
 
 }
---
+++ src/test/java/com/dutytrail/frontend/api/server/AvailabilityServiceTest.java
@@ -0,0 +1,45 @@
+package com.dutytrail.frontend.api.server;
+
+import com.dutytrail.frontend.api.entity.Status;
+import com.dutytrail.frontend.api.remote.DutyClient;
+import com.dutytrail.frontend.api.remote.TrailClient;
+import com.dutytrail.frontend.api.starter.Api;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.SpringApplicationContextLoader;
+import org.springframework.boot.test.TestRestTemplate;
+import org.springframework.boot.test.WebIntegrationTest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.web.client.RestTemplate;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = Api.class, loader = SpringApplicationContextLoader.class)
+@WebIntegrationTest({"eureka.client.enabled=false","spring.cloud.config.enabled=false", "spring.profiles.active=test"})
+public class AvailabilityServiceTest {
+
+    @Mock private DutyClient dutyClient;
+    @Mock private TrailClient trailClient;
+    private RestTemplate restTemplate = new TestRestTemplate();
+
+    @Before
+    public void setUp(){
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void statusTest(){
+        ResponseEntity<Status> statusResponseEntity = restTemplate.getForEntity("http://localhost:9999/status", Status.class);
+        Assert.assertNotNull(statusResponseEntity);
+        Status status = statusResponseEntity.getBody();
+        Assert.assertNotNull(status);
+        Assert.assertEquals("Config properties loaded from LOCAL profile", status.getConfigPing());
+        Assert.assertEquals("Duty Service Alive. Profile in use: Config properties loaded from HEROKU profile", status.getDutyServicePing());
+        Assert.assertEquals("Trail Service Alive. Profile in use: Config properties loaded from HEROKU profile", status.getTrailServicePing());
+    }
+}
