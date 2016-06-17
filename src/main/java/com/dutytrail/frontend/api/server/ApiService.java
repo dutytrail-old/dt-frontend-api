187fed41ec64f9a95e292116d9455e868b0d9c2e
joan.tolos <joan.tolos@thomsonreuters.com>
2016-06-17 12:40:29 +0200

    Trails working on API

--- src/main/java/com/dutytrail/frontend/api/server/ApiService.java
+++ src/main/java/com/dutytrail/frontend/api/server/ApiService.java
@@ -4,6 +4,7 @@ import com.dutytrail.frontend.api.entity.ApiOutput;
 import com.dutytrail.frontend.api.entity.DutyInput;
 import com.dutytrail.frontend.api.remote.DutyClient;
 import com.dutytrail.frontend.api.remote.TrailClient;
+import com.dutytrail.frontend.api.remote.entity.Duty;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
